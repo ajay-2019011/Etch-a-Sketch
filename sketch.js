fe7f16b67042d8b3419b558ba0e732e830ac7bbb
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-23 18:47:25 +0530

    Removed the unnecessary borders and added the function for generating random colors

--- sketch.js
+++ sketch.js
@@ -12,7 +12,13 @@ let clickedErase = 0,clickedColor=1,clickedRainbow=0;
 let i = 0;
 let wide = 800 / total;
 
-
+let values = ["lime", "orange", "purple", "indigo", "green", "gold", "blue", "brown", "red", "aqua"];
+function randomColor() {
+  let min = 0;
+  let max = 10;
+  let randomInt = Math.floor(Math.random() * (max - min)) + min;
+  return values[randomInt];
+}
 
 for (i = 0; i < total;i++){
   let element = document.createElement('div');

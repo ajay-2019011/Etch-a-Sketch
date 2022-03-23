32266f2ae447c085d7a360c97f8493c8e152df68
ajay-2019011 <ajaysheoran2019011@gmail.com>
2022-03-23 18:50:00 +0530

    Added Event listener for each button and the conditions for which those will work

--- sketch.js
+++ sketch.js
@@ -6,9 +6,9 @@
 //const test = document.getElementById("test");
 //test.remove();
 //--------------XXXXXX--------------------
-let total = prompt("Please enter the size of matrix",16);
+let total = prompt("Please enter the size of matrix", 16);
 const board = document.querySelector('.board');
-let clickedErase = 0,clickedColor=1,clickedRainbow=0;
+let clickedErase = 0, clickedColor = 1, clickedRainbow = 0;
 let i = 0;
 let wide = 800 / total;
 
@@ -20,7 +20,47 @@ function randomColor() {
   return values[randomInt];
 }
 
-for (i = 0; i < total;i++){
+const erase = document.querySelector('#erase');
+erase.addEventListener('click', eraseIt);
+
+const color = document.querySelector('#color');
+color.addEventListener('click', colorIt);
+
+const rainbow = document.querySelector('#rainbow');
+rainbow.addEventListener('click', rainbowIt);
+
+
+function eraseIt(e) {
+  clickedErase = 1;
+  clickedColor = 0;
+  clickedRainbow = 0;
+}
+
+function colorIt(e) {
+  clickedColor = 1;
+  clickedErase = 0;
+  clickedRainbow = 0;
+}
+
+function rainbowIt(e) {
+  clickedRainbow = 1;
+  clickedErase = 0;
+  clickedColor = 0;
+}
+
+function check(val) {
+  if (val == 0) {
+    return clickedErase;
+  }
+  else if (val == 1) {
+    return clickedColor;
+  }
+  else if (val == 2) {
+    return clickedRainbow;
+  }
+}
+
+for (i = 0; i < total; i++) {
   let element = document.createElement('div');
   element.classList.add("in");
   board.appendChild(element);
@@ -31,7 +71,14 @@ inElements.forEach((inElement) => {
   for (i = 0; i < total; i++) {
     let ele = document.createElement('div');
     ele.classList.add("into");
-    
+    ele.addEventListener('mouseover', () => {
+      if (check(0))
+        ele.style.backgroundColor = "white";
+      else if (check(1))
+        ele.style.backgroundColor = "black";
+      else if (check(2))
+        ele.style.backgroundColor = randomColor();
+    });
     inElement.appendChild(ele);
   }
 });
